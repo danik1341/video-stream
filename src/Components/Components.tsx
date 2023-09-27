fe16b8d7a9121b311e9ca2f30588425c6ca11548
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-09-27 12:43:06 +0300

    video comp multi video update

--- src/Components/Components.tsx
+++ src/Components/Components.tsx
@@ -2,4 +2,6 @@ export { default as Navbar } from "./Navbar";
 export { default as Sidebar } from "./Sidebar";
 export { default as Layout } from "./Layout";
 export { default as Footer } from "./Footer";
-export { UserImage } from "./VideoComponent";
+export { default as Thumbnail } from "./Thumbnail";
+export { LoadingSkeleton, ErrorMessage } from "./ErrorMessage";
+export { UserImage, MultiColumnVideo } from "./VideoComponent";
--- src/Components/VideoComponent.tsx
+++ src/Components/VideoComponent.tsx
@@ -1,4 +1,104 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Thumbnail } from "./Components";
+import moment from "moment";
+
+interface VideoComponentProps {
+  videos: {
+    id: string;
+    title: string;
+    thumbnailUrl: string;
+    createdAt: Date;
+    views: number;
+  }[];
+  users: {
+    image: string;
+    name: string;
+  }[];
+  refetch?: () => Promise<unknown>;
+}
+
+export const VideoTitle = ({
+  title,
+  limitHeight,
+  limitSize,
+}: {
+  title: string;
+  limitHeight?: boolean;
+  limitSize?: boolean;
+}) => {
+  return (
+    <h1
+      className={` max-w-md font-semibold leading-6 text-gray-900 group-hover:text-gray-600 ${
+        limitSize ? "text-base" : "text-lg"
+      } ${limitHeight ? "max-h-12 w-full overflow-hidden" : ""}`}
+    >
+      {title}
+    </h1>
+  );
+};
+
+export const VideoInfo = ({
+  views,
+  createdAt,
+}: {
+  views: number;
+  createdAt: Date | string;
+}) => {
+  return (
+    <div className=" mt-1 flex max-h-6 items-start overflow-hidden text-sm">
+      <p className=" text-gray-600">
+        {views}
+        <span> Views</span>
+      </p>
+      <li className=" pl-2 text-sm text-gray-500"></li>
+      <p className=" text-gray-600">{moment(createdAt).fromNow()}</p>
+    </div>
+  );
+};
+
+export const UserName = ({ name }: { name: string }) => {
+  return (
+    <p className=" max-h-6 overflow-hidden text-sm font-semibold leading-6 text-gray-900">
+      {name}
+    </p>
+  );
+};
+
+export const MultiColumnVideo: React.FC<VideoComponentProps> = ({
+  videos,
+  users,
+}) => (
+  <div className=" mx-auto grid grid-cols-1 gap-x-4 gap-y-8 md:mx-0 md:max-w-none md:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:max-w-none xl:grid-cols-3 2xl:mx-0 2xl:max-w-none 2xl:grid-cols-3">
+    {videos.map((video, index) => {
+      const user = users[index];
+      if (!user) {
+        return null;
+      }
+      return (
+        <Link
+          href={`/video/${video.id}`}
+          className="flex flex-col items-start justify-between hover:rounded-md hover:bg-gray-100"
+          key={video.id}
+        >
+          <div className=" relative w-full">
+            <Thumbnail thumbnailUrl={video.thumbnailUrl} />
+            <div className="max-w-xl">
+              <div className=" relative mt-4 flex items-start gap-x-4">
+                <UserImage image={user.image || ""} />
+                <div className="w-full">
+                  <VideoTitle title={video.title} limitSize={true} />
+                  <VideoInfo views={video.views} createdAt={video.createdAt} />
+                  <UserName name={user.name} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Link>
+      );
+    })}
+  </div>
+);
 
 export function UserImage({
   image,
