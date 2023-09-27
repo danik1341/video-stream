ad01bb16e52d43ca8d29d941c2518b8c45a02f40
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-09-27 14:15:38 +0300

    all video renders done

--- src/Components/Components.tsx
+++ src/Components/Components.tsx
@@ -4,4 +4,9 @@ export { default as Layout } from "./Layout";
 export { default as Footer } from "./Footer";
 export { default as Thumbnail } from "./Thumbnail";
 export { LoadingSkeleton, ErrorMessage } from "./ErrorMessage";
-export { UserImage, MultiColumnVideo } from "./VideoComponent";
+export {
+  UserImage,
+  MultiColumnVideo,
+  SingleColumnVideo,
+  SmallSingleColumnVideo,
+} from "./VideoComponent";
