bf1e7898f64ca4fdae0e47236161c49b57a70cda
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-09-27 14:15:57 +0300

    search page done

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,69 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import {
+  ErrorMessage,
+  Layout,
+  LoadingSkeleton,
+  SingleColumnVideo,
+} from "~/Components/Components";
+
+import { api } from "~/utils/api";
+
+const SearchPage: NextPage = () => {
+  const router = useRouter();
+  const searchQuery = router.query.q;
+  const { data, isLoading, error } = api.video.getVideoBySearch.useQuery(
+    searchQuery as string,
+  );
+
+  const Error = () => {
+    if (isLoading) {
+      return <LoadingSkeleton count={40} />;
+    } else if (error ?? !data) {
+      return (
+        <ErrorMessage
+          icon="GreenPlay"
+          message="No search results found"
+          description="Sorry try another search result."
+        />
+      );
+    } else {
+      return <></>;
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>VidChill</title>
+        <meta name="description" content="Video Streaming At VidChill" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout>
+        {!data || error ? (
+          <Error />
+        ) : (
+          <>
+            <SingleColumnVideo
+              videos={data.videos.map((video) => ({
+                id: video?.id ?? "",
+                title: video?.title ?? "",
+                thumbnailUrl: video?.thumbnailUrl ?? "",
+                createdAt: video?.createdAt ?? new Date(),
+                views: video?.views ?? 0,
+              }))}
+              users={data.users.map((user) => ({
+                name: user?.name ?? "",
+                image: user?.image ?? "",
+              }))}
+            />
+          </>
+        )}
+      </Layout>
+    </>
+  );
+};
+
+export default SearchPage;
